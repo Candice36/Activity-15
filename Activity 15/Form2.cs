e8ef5731bc62461625a427fe7bd995aceb63c61b
Candice36 <candicebell@yahoo.com>
2021-08-11 22:47:53 -0500

    Add project files.

---
+++ Activity 15/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_15
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+            Form1 frm = new Form1();
+
+            label2.Text = frm.getLuckyNumber().ToString();
+        }
+    }
+}
+
+//End the Code of Form2
